e52c61b05a0b9ac253815b89b778a8d832cf4a3b
luka <lukakapanadze04@gmail.com>
2024-03-29 13:12:03 +0400

    tailwind

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,23 +1,23 @@
-import Header from "./Header";
-import Loader from "./Loader";
-import CartOverview from "../features/cart/CartOverview";
-import { Outlet, useNavigation } from "react-router-dom";
+import Header from './Header'
+import Loader from './Loader'
+import CartOverview from '../features/cart/CartOverview'
+import { Outlet, useNavigation } from 'react-router-dom'
 
 function AppLayout() {
-  const navigation = useNavigation();
-  const isLoading = navigation.state === "loading";
+    const navigation = useNavigation()
+    const isLoading = navigation.state === 'loading'
 
-  return (
-    <div className="layout">
-      {isLoading && <Loader />}
+    return (
+        <div className="grid h-screen  grid-rows-[auto_1fr_auto] ">
+            {isLoading && <Loader />}
 
-      <Header />
-      <main>
-        <Outlet />
-      </main>
-      <CartOverview />
-    </div>
-  );
+            <Header />
+            <main>
+                <Outlet />
+            </main>
+            <CartOverview />
+        </div>
+    )
 }
 
-export default AppLayout;
+export default AppLayout
